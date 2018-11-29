d119b667bbd48d43066abb8da03cd8715387c776
deep_learning <34463783+Coder-WangZe@users.noreply.github.com>
2018-11-29 19:43:17 +0800

    Add files via upload

--- TestSavedModel.py
+++ TestSavedModel.py
@@ -3,57 +3,21 @@ import sys
 sys.path.insert(1, 'D:\\project3_faster_rcnn\\models-master\\research\\')
 os.chdir('D:\\project3_faster_rcnn\\models-master\\research\\')
 
-from timeit import default_timer as timer
 import numpy as np
 import tensorflow as tf
 from time import time
 from matplotlib import pyplot as plt
-from PIL import Image
-
-from object_detection.utils import ops as utils_ops
+import cv2
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
 
 
-def load_image_into_numpy_array(image):
-    (im_width, im_height) = image.size
-    return np.array(image.getdata()).reshape(
-      (im_height, im_width, 3)).astype(np.uint8)
+def run_inference_for_single_image(image, sess, tensor_dict):
 
-
-def run_inference_for_single_image(image, sess):
     # Get handles to input and output tensors
-    ops = tf.get_default_graph().get_operations()
-    all_tensor_names = {output.name for op in ops for output in op.outputs}
-    tensor_dict = {}
-    for key in [
-      'num_detections', 'detection_boxes', 'detection_scores',
-      'detection_classes', 'detection_masks'
-    ]:
-        tensor_name = key + ':0'
-        if tensor_name in all_tensor_names:
-            tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
-              tensor_name)
-    if 'detection_masks' in tensor_dict:
-        # The following processing is only for single image
-        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
-        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
-        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
-        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
-        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
-        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
-        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
-            detection_masks, detection_boxes, image.shape[0], image.shape[1])
-        detection_masks_reframed = tf.cast(
-            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
-        # Follow the convention by adding back the batch dimension
-        tensor_dict['detection_masks'] = tf.expand_dims(
-            detection_masks_reframed, 0)
     image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
-
     # Run inference
     output_dict = sess.run(tensor_dict, feed_dict={image_tensor: np.expand_dims(image, 0)})
-
     # all outputs are float32 numpy arrays, so convert types as appropriate
     output_dict['num_detections'] = int(output_dict['num_detections'][0])
     output_dict['detection_classes'] = output_dict[
@@ -69,16 +33,20 @@ def detect_image(path, image_output_path, detection_graph):
     time1 = time()
     with detection_graph.as_default():
         with tf.Session() as sess:
+            ops = tf.get_default_graph().get_operations()
+            all_tensor_names = {output.name for op in ops for output in op.outputs}
+            tensor_dict = {}
+            for key in ['num_detections', 'detection_boxes', 'detection_scores', 'detection_classes']:
+                tensor_name = key + ':0'
+                if tensor_name in all_tensor_names:
+                    tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+                        tensor_name)
             image_paths = [path + name for name in os.listdir(path)]
             for image_path in image_paths:
-                image = Image.open(image_path)
-                # the array based representation of the image will be used later in order to prepare the
                 # result image with boxes and labels on it.
-                image_np = load_image_into_numpy_array(image)
-                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-                image_np_expanded = np.expand_dims(image_np, axis=0)
+                image_np = cv2.imread(image_path)
                 # Actual detection.
-                output_dict = run_inference_for_single_image(image_np, sess)
+                output_dict = run_inference_for_single_image(image_np, sess, tensor_dict)
                 # Visualization of the results of a detection.
                 vis_util.visualize_boxes_and_labels_on_image_array(
                     image_np,
@@ -89,7 +57,7 @@ def detect_image(path, image_output_path, detection_graph):
                     instance_masks=output_dict.get('detection_masks'),
                     use_normalized_coordinates=True,
                     line_thickness=5)
-                plt.figure(figsize=IMAGE_SIZE)
+                plt.figure(figsize=(12, 8))
                 plt.imshow(image_np)
                 plt.savefig(image_output_path + 'test_' + os.path.split(image_path)[1])
     process_time = round((time() - time1) / 60, 2)
@@ -97,7 +65,7 @@ def detect_image(path, image_output_path, detection_graph):
 
 
 def detect_video(video_path, output_path=""):
-    import cv2
+    time1 = time()
     vid = cv2.VideoCapture(video_path)
     if not vid.isOpened():
         raise IOError("Couldn't open webcam or video")
@@ -112,20 +80,24 @@ def detect_video(video_path, output_path=""):
     accum_time = 0
     curr_fps = 0
     fps = "FPS: ??"
-    prev_time = timer()
+    prev_time = time()
     with detection_graph.as_default():
         with tf.Session() as sess:
+            ops = tf.get_default_graph().get_operations()
+            all_tensor_names = {output.name for op in ops for output in op.outputs}
+            tensor_dict = {}
+            for key in ['num_detections', 'detection_boxes', 'detection_scores', 'detection_classes']:
+                tensor_name = key + ':0'
+                if tensor_name in all_tensor_names:
+                    tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+                        tensor_name)
             while True:
-                return_value, frame = vid.read()
-                image = Image.fromarray(frame)
+                return_value, image_np = vid.read()
                 ###################
-                image_np = load_image_into_numpy_array(image)
-                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-                image_np_expanded = np.expand_dims(image_np, axis=0)
                 # Actual detection.
-                output_dict = run_inference_for_single_image(image_np, sess)
+                output_dict = run_inference_for_single_image(image_np, sess, tensor_dict)
                 # Visualization of the results of a detection.
-                vis_util.visualize_boxes_and_labels_on_image_array(
+                result = vis_util.visualize_boxes_and_labels_on_image_array(
                     image_np,
                     output_dict['detection_boxes'],
                     output_dict['detection_classes'],
@@ -135,8 +107,7 @@ def detect_video(video_path, output_path=""):
                     use_normalized_coordinates=True,
                     line_thickness=5)
                 #######################
-                result = np.asarray(image_np)
-                curr_time = timer()
+                curr_time = time()
                 exec_time = curr_time - prev_time
                 prev_time = curr_time
                 accum_time = accum_time + exec_time
@@ -153,6 +124,7 @@ def detect_video(video_path, output_path=""):
                     out.write(result)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
+    print("cost time: %.2f min" % ((time()-time1)/60))
 
 
 models = ['faster_rcnn_resnet50', 'faster_rcnn_resnet101', 'faster_rcnn_inception_v2']
@@ -170,14 +142,11 @@ with detection_graph.as_default():
         tf.import_graph_def(od_graph_def, name='')
 category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
 
-path_to_test_images_dir = "D:\\project3_faster_rcnn\\models-master\\research\\hat_dataset\\test_image\\"
 
-IMAGE_SIZE = (12, 8)
+images_dir = "D:\\project3_faster_rcnn\\models-master\\research\\hat_dataset\\test_image\\"
 image_output_path = "D:\\project3_faster_rcnn\\models-master\\research\\hat_dataset\\test_output\\" + models[i]+"\\"
-if os.path.exists(image_output_path) == 0:
-    os.makedirs(image_output_path)
+detect_image(images_dir, image_output_path, detection_graph)
 
-# detect_image(path_to_test_images_dir, image_output_path, detection_graph)
 video_path = "D:\\project3_faster_rcnn\\models-master\\research\\hat_dataset\\video_data\\test_video\\1.mp4"
 output_path = "D:\\project3_faster_rcnn\\models-master\\research\\hat_dataset\\video_data\\test_video\\1_"+models[i]+".mp4"
 detect_video(video_path, output_path)
