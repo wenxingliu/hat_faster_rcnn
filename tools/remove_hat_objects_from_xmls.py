53057807f0bee69058bc19b901f70370dc1e7c06
Coder-WangZe <15313001325@163.com>
2019-01-18 22:01:22 +0800

    add tools, extract frames and remove hat from xmls

---
+++ tools/remove_hat_objects_from_xmls.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 18 20:57:14 2019
+
+@author: admin
+"""
+
+all_xmls_path = "C:\\Users\\admin\\Desktop\\xmls"
+
+import os
+import xml.etree.ElementTree as ET
+
+#os.chdir(all_xmls_path)
+
+xml_list = os.listdir(all_xmls_path)
+
+for id, xml in enumerate(xml_list):
+    xml_path = os.path.join(all_xmls_path, xml)
+    
+
+    xml_tree = ET.parse(xml_path)
+    
+    root = xml_tree.getroot()
+    
+    
+    object_bboxes = root.findall("object")
+    for i in range(len(object_bboxes)):
+        bbox = root.find("object")
+        if bbox.find('name').text == "hat":
+            root.remove(bbox)
+            
+        
+        
+    xml_tree.write(os.path.join(all_xmls_path, str(id) +".xml"))
+
+
