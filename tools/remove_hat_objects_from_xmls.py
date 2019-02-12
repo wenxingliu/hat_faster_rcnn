48dc6f451c1703736ffc4b06307cffdac5378596
Coder-WangZe <15313001325@163.com>
2019-02-12 22:12:20 +0800

    update

--- tools/remove_hat_objects_from_xmls.py
+++ tools/remove_hat_objects_from_xmls.py
@@ -5,7 +5,8 @@ Created on Fri Jan 18 20:57:14 2019
 @author: admin
 """
 
-all_xmls_path = "D:\\project3_faster_rcnn\\models-master\\research\\hat_dataset\\data_single_no_hat\\train_xmls"
+# all_xmls_path = "C:\\Users\\admin\\Desktop\\xmls"
+all_xmls_path = "D:\\project3_faster_rcnn\\models-master\\research\\hat_dataset\\data_single_no_hat\\test_xmls"
 
 import os
 import xml.etree.ElementTree as ET
@@ -16,19 +17,17 @@ xml_list = os.listdir(all_xmls_path)
 
 for id, xml in enumerate(xml_list):
     xml_path = os.path.join(all_xmls_path, xml)
-    
 
     xml_tree = ET.parse(xml_path)
     
     root = xml_tree.getroot()
-    
-    
+
     object_bboxes = root.findall("object")
-    for i in range(len(object_bboxes)):
-        bbox = root.find("object")
-        if bbox.find('name').text == "hat":
-            root.remove(bbox)
-            
+
+    for ob in object_bboxes:
+        if ob.find("name").text == "hat":
+            root.remove(ob)
+
     xml_tree.write(xml_path)
 
 
